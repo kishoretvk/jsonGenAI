9a3454ee1c89bd2f805ca876bf6310017325d8db
krishna kishore <kishoretvk@hotmail.com>
2024-09-16 23:07:46 -0500

    updated uuid

--- jsonAI/main.py
+++ jsonAI/main.py
@@ -2,9 +2,6 @@ from typing import List, Set, Union, Dict, Any
 from datetime import datetime, date, time
 import uuid
 import base64
-
-
-
 from jsonAI.logits_processors import (
     NumberStoppingCriteria,
     OutputNumbersTokens,
@@ -94,9 +91,6 @@ class Jsonformer:
         self.debug("[generate_binary]", prompt, is_prompt=True)
         return base64.b64encode(b"example binary data").decode('utf-8')
     
-    
-
-
     def generate_number(self, temperature: Union[float, None] = None, iterations=0):
         prompt = self.get_prompt()
         self.debug("[generate_number]", prompt, is_prompt=True)
@@ -128,6 +122,35 @@ class Jsonformer:
                 raise ValueError("Failed to generate a valid number")
 
             return self.generate_number(
+                temperature=self.temperature * 1.3, iterations=iterations + 1
+            )
+    def generate_uuid(self, temperature: Union[float, None] = None, iterations=0):
+        prompt = self.get_prompt()
+        self.debug("[generate_uuid]", prompt, is_prompt=True)
+        input_tokens = self.tokenizer.encode(prompt, return_tensors="pt").to(
+            self.model.device
+        )
+        response = self.model.generate(
+            input_tokens,
+            max_new_tokens=self.max_number_tokens,
+            num_return_sequences=1,
+            temperature=temperature or self.temperature,
+            pad_token_id=self.tokenizer.eos_token_id,
+        )
+        response = self.tokenizer.decode(response[0], skip_special_tokens=True)
+
+        response = response[len(prompt):]
+        response = response.replace(" ", "").strip()
+        self.debug("[generate_uuid]", response)
+        try:
+            # Validate the generated UUID
+            uuid_obj = uuid.UUID(response)
+            return str(uuid_obj)
+        except ValueError:
+            if iterations > 3:
+                raise ValueError("Failed to generate a valid UUID")
+
+            return self.generate_uuid(
                 temperature=self.temperature * 1.3, iterations=iterations + 1
             )
 
